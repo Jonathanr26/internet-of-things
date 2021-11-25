1dc7eef5419821193d0541d96e9a54af3b0b47d5
Jonathanr26 <jrodriguez110@ucol.mx>
2021-11-25 09:48:15 -0600

    Implementación  de Express Gateway

---
+++ express-gateway/iot-services/users.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+let app = express();
+
+app.get('/users', (req, res, next) => {
+	let users = [
+		'Nikola Tesla',
+		'James Maxwell',
+		'JOseph Fourier',
+	];
+
+	res.send(users);
+});
+
+app.listen(3001, () => {
+	console.log('App running on port 3001');
+});
